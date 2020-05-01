3cb82918d2c31332b8f32630e9ede53cac6accfe
lavsexpert <lavrov-sergey@yandex.ru>
2020-05-01 23:59:13 +0300

    Сделано меню

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void StartPressed()
+    {
+        SceneManager.LoadScene("Profile");
+        Debug.Log("Start pressed!");
+    }
+
+    public void RestorePressed()
+    {
+        Debug.Log("Restore pressed!");
+    }
+
+    public void ExitPressed()
+    {
+        Application.Quit();
+        Debug.Log("Exit pressed!");
+    }
+}
