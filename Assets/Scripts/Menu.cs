f78520ea298d40f039922012e32462f06fbd77a1
lavsexpert <lavrov-sergey@yandex.ru>
2020-05-02 02:57:41 +0300

    Доработано меню и сделаны первые сборки

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,22 +1,40 @@
-﻿using UnityEngine.SceneManagement;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    public Button startButton, restoreButton, exitButton;
+
+    void Start()
+    {
+        Debug.Log("OnStart()");
+        startButton.onClick.AddListener(() => StartPressed());
+        restoreButton.onClick.AddListener(() => RestorePressed());
+        exitButton.onClick.AddListener(() => ExitPressed());
+    }
+
     public void StartPressed()
     {
         SceneManager.LoadScene("Profile");
-        Debug.Log("Start pressed!");
+        Debug.Log("StartPressed()");
     }
 
     public void RestorePressed()
     {
-        Debug.Log("Restore pressed!");
+        Debug.Log("RestorePressed()");
     }
 
     public void ExitPressed()
     {
         Application.Quit();
-        Debug.Log("Exit pressed!");
+        Debug.Log("ExitPressed()");
+    }
+
+    public void Destroy()
+    {
+        startButton.onClick.RemoveListener(() => StartPressed());
+        restoreButton.onClick.RemoveListener(() => RestorePressed());
+        exitButton.onClick.RemoveListener(() => ExitPressed());
     }
 }
